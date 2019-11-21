a75a1dfd8db11122d2557b2b5984373d925e89a9
Raul Dipeas <rauldipeas@mail.com>
2019-11-21 04:10:48 -0300

    ^download-counter

--- website/download-counter/download-counter.sh
+++ website/download-counter/download-counter.sh
@@ -4,5 +4,5 @@ wget -q https://raw.githubusercontent.com/mmilidoni/github-downloads-count/maste
 git clone https://github.com/rauldipeas/Unity-XP;cd Unity-XP;mkdir -pv website/download-counter
 cp ../download-counter.log website/download-counter/download-counter.log
 git config --global user.email "rauldipeas@mail.com";git config --global user.name "Raul Dipeas"
-git add .; git commit -m "Azure Pipelines auto build"
+git add .; git commit -m "^download-counter"
 git push http://rauldipeas:$GITHUB_ACCESS_TOKEN@github.com/rauldipeas/Unity-XP.git HEAD:master
