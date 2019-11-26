3c34dad22b51a5e00575dcdff350f5d0837712f6
Raul Dipeas <rauldipeas@mail.com>
2019-11-26 02:30:26 -0300

    ^website(gif-background)

--- website/js/script.js
+++ website/js/script.js
@@ -61,7 +61,7 @@ $(function() {
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000, function() {
+                }, 300, function() {
                     // Callback after animation
                     // Must change focus!
                     var $target = $(target);
