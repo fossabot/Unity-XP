9bd6ca2120bbe99a84b456084dd5ccaf5993b3af
Raul Dipeas <rauldipeas@mail.com>
2019-11-29 17:55:50 -0300

    ^website

--- website/download-counter/download-counter.sh
+++ website/download-counter/download-counter.sh
@@ -1,6 +1,3 @@
 pip install requests
 wget -q https://raw.githubusercontent.com/mmilidoni/github-downloads-count/master/gdc;chmod +x -v gdc
 ./gdc rauldipeas Unity-XP | tac | awk 'NR <=1' | awk '{print $1}'  | echo +$(xargs expr 600 +) Downloads > website/download-counter/download-counter.log;rm -rfv gdc
-#git config --global user.email "rauldipeas@mail.com";git config --global user.name "Raul Dipeas"
-#git add .; git commit -m "^download-counter"
-#git push http://rauldipeas:$GITHUB_ACCESS_TOKEN@github.com/rauldipeas/Unity-XP.git HEAD:website
