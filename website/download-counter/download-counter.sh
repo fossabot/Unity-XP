b74dd77aa1982b76647d37f314b5fee16d1f96b6
Raul Dipeas <rauldipeas@mail.com>
2019-11-29 20:04:53 -0300

    ^website

--- website/download-counter/download-counter.sh
+++ website/download-counter/download-counter.sh
@@ -1,3 +1,3 @@
 pip install requests
-wget -q https://raw.githubusercontent.com/mmilidoni/github-downloads-count/master/gdc;chmod +x -v gdc
+wget https://raw.githubusercontent.com/mmilidoni/github-downloads-count/master/gdc;chmod +x -v gdc
 ./gdc rauldipeas Unity-XP | tac | awk 'NR <=1' | awk '{print $1}'  | echo +$(xargs expr 600 +) Downloads | tee website/download-counter/download-counter.log;rm -rfv gdc
